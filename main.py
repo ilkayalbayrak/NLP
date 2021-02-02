bbc68f2ba984e09d6cd5cccdc5e4810c3cd48784
ilkayalbayrak <ilkayyalbayrak@gmail.com>
2021-02-03 00:15:34 +0100

    Added sentiment score normalizer

--- main.py
+++ main.py
@@ -169,6 +169,17 @@ def penn_to_wn(penntag, return_none=False, default_to_noun=True):
         else:
             return ''
 
+def normalize_sentiment_score(sentiment_sum, tokens_count):
+    if tokens_count == 0:
+        return 0
+    print("\n-------- [INFO]: FINAL SENT SCORE --------\nSentiment: {}, Token Count: {}".format(sentiment_sum, tokens_count))
+    sentiment = sentiment_sum / tokens_count
+    print("sentiment/tokens_count == {}".format(sentiment))
+    if sentiment >= 0.01:
+        return 1
+    if sentiment <= -0.01:
+        return -1
+    return 0
 
 
 def sentiment_sentiwordnet(text):
@@ -176,7 +187,7 @@ def sentiment_sentiwordnet(text):
     raw_sentences = sent_tokenize(text)
     print("\n-------- [INFO]: RAW SENTENCES, TOKENIZE REVIEW TO SENTENCES --------\n{}".format(raw_sentences))
 
-    sentiment = 0
+    sentiment_sum = 0
     tokens_count = 0
 
     for raw_sentence in raw_sentences:
@@ -187,16 +198,16 @@ def sentiment_sentiwordnet(text):
             raw_sentence)  # Replacing Negations with Antonyms (Python 3 Text Processing with NLTK 3 Cookbook)
         print("\n-------- [INFO]: REPLACE NEGATIONS --------\n{}".format(raw_sentence))
 
-        tokenizedWords = word_tokenize(raw_sentence)
-        print("\n-------- [INFO]: TOKENIZE TO WORDS --------\n{}".format(tokenizedWords))
+        tokenized_words = word_tokenize(raw_sentence)
+        print("\n-------- [INFO]: TOKENIZE TO WORDS --------\n{}".format(tokenized_words))
 
-        tagged_sentence = pos_tag(tokenizedWords)
+        tagged_sentence = pos_tag(tokenized_words)
         print("\n-------- [INFO]: POS TAGGING --------\n{}".format(tagged_sentence))
 
-        stopWordsRemoved = [w for w in tagged_sentence if not w[0] in stop_words]
-        print("\n-------- [INFO]: REMOVE STOPWORDS --------\n{}".format(stopWordsRemoved))
+        tagged_sentence_without_stopwords = [w for w in tagged_sentence if not w[0] in stop_words]
+        print("\n-------- [INFO]: REMOVE STOPWORDS --------\n{}".format(tagged_sentence_without_stopwords))
 
-        for word, tag in tagged_sentence:
+        for word, tag in tagged_sentence_without_stopwords:
             wn_tag = penn_to_wn(tag)
 
             # print("\n#################### -------- [INFO]: STEMMING RESULT -------- ###################\n{}".format(stemmer.stem(word)))
@@ -218,23 +229,31 @@ def sentiment_sentiwordnet(text):
                     lemma, word_sent))
 
             if word_sent != 0:
-                sentiment += word_sent
+                sentiment_sum += word_sent
                 tokens_count += 1
 
-    if tokens_count == 0:
-        return 0
-    print("\n-------- [INFO]: FINAL SENT SCORE --------\nSentiment: {}, Token Count: {}".format(sentiment, tokens_count))
-    sentiment = sentiment / tokens_count
-    print("sentiment/tokens_count == {}".format(sentiment))
+    normalize_sentiment_score(sentiment_sum, tokens_count)
+    # if tokens_count == 0:
+    #     return 0
+    # print("\n-------- [INFO]: FINAL SENT SCORE --------\nSentiment: {}, Token Count: {}".format(sentiment, tokens_count))
+    # sentiment = sentiment / tokens_count
+    # print("sentiment/tokens_count == {}".format(sentiment))
+    #
+    # if sentiment >= 0.01:
+    #     return 1
+    # if sentiment <= -0.01:
+    #     return -1
+    # return 0
 
-    if sentiment >= 0.01:
-        return 1
-    if sentiment <= -0.01:
-        return -1
-    return 0
 
+print(sentiment_sentiwordnet("I don't care. He shouldn't have done that. I haven't received it yet. I am not happy. She isn't consistent."))
+# print(sentiment_sentiwordnet(data_df["reviewText"][25]))
+# print(sentiment_sentiwordnet("I really like the new design of your website! I’m not sure if I like the new design. The new design is awful!"))
+# print(sentiment_sentiwordnet("But, as with so many retail stars, GameStop began to struggle a decade or so ago as gamers, like everybody else, made more of their purchases on the internet, opting for downloaded games or two-day delivery over a visit to the mall."))
+# print(wn.synset)
 
-sentiment_sentiwordnet(data_df["reviewText"][1])
+# sentiment_sentiwordnet(data_df["reviewText"][1])
 
 # def sentiment_analysis(review_text):
 
+# print(data_df["overall"])
